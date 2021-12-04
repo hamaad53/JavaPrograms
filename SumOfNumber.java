cf2833d95e39cdc2cb932164b56521ac93c2c628
hamaad53 <95532518+hamaad53@users.noreply.github.com>
2021-12-04 18:45:59 +0530

    Create SumOfNumber.java

---
+++ SumOfNumber.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers 
+{  
+public static void main(String args[])   
+{  
+int num1 = 133, num2 = 110, sum;  
+sum = num1 + num2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+} 
