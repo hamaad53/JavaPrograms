28fd78e1982aa3b2128e55700cc7e9fc12169274
hamaad53 <95532518+hamaad53@users.noreply.github.com>
2021-12-04 19:02:03 +0530

    Create GenerateNumber.java

---
+++ GenerateNumber.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.ThreadLocalRandom;
+  
+class GrnerateNumber {
+  
+    public static int getRandomValue(int Min, int Max)
+    {
+  
+        return ThreadLocalRandom
+            .current()
+            .nextInt(Min, Max + 1);
+    }
+  
+    public static void main(String[] args)
+    {
+  
+        int Min = 1, 
+     Scanner sc = new Scanner(System.in);
+     int Max  = sc.nextInt();;
+  
+        System.out.println("Random value between "
+                           + Min + " and " + Max + ": "
+                           + getRandomValue(Min, Max));
+    }
+}
